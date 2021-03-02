4a18df8475803a207274fb4b2b7e7af423cf9298
James Ma <jamesm@rockhopper-04.cs.wisc.edu>
2021-03-01 22:02:17 -0600

    complete implementation for output redirection, 23/37 tests passed

--- mysh.c
+++ mysh.c
@@ -5,14 +5,18 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <errno.h>
 
 FILE *stdin;
-FILE *fp;
+FILE *fp1;
+FILE *fp2;
+int fd;
+int old_stdout;
 
 // this function runs an individual line containing a user command
 // takes in string of user command, parses arguments, and calls execv
-void handle_command(const char *user_command){
-    int fd; // file descriptor for file to redirect to
+void handle_command(const char *user_command) {
+    // file descriptor for file to redirect to
     // make duplicate of command
     char *dup_command = strdup(user_command);
     // remove newline
@@ -23,73 +27,108 @@ void handle_command(const char *user_command){
     }
     // case: valid command
     if (strlen(dup_command) > 0) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            //printf("I'm a child with pid %d.\n", getpid());
-            
-            // detect redirection
-            int start_of_filename = 0;
-            bool redirect_detected = false;
-            for (int i = 0; i < strlen(dup_command); i++) {
-                if (dup_command[i] == '>') {
-                    // check for multiple >'s
-                    if (redirect_detected) {
-                         write(STDERR_FILENO, "Redirection misformatted.\n", 26);
-                    } else {
-                        redirect_detected = true;
-                    }
+    // detect redirection
+        int start_of_filename = 0;
+        bool redirect_detected = false;
+        int redirect_index;
+        for (int i = 0; i < strlen(dup_command); i++) {
+            if (dup_command[i] == '>') {
+                // check for multiple >'s
+                if (redirect_detected) {
+                    write(STDERR_FILENO, "Redirection misformatted.\n", 26);
+                } else {
+                    redirect_detected = true;
+                    redirect_index = i;
                 }
-                // skip spaces and tabs
-                else if (dup_command[i] == ' ' || dup_command[i] == '\t') {
-                    continue;
-                } 
-                // find potential beginning of filename
-                else {
-                    // if after > symbol, mark as beginning of filename
-                    if (redirect_detected) {
+            } else if (dup_command[i] == ' ' || dup_command[i] == '\t') {
+                continue;
+            } else {
+                // if after > symbol, mark as beginning of filename
+                if (redirect_detected) {
+                    if (dup_command[i] != ' ' || dup_command[i] != '\t') {
                         start_of_filename = i;
                         break;
-                    } else {
-                        continue;
                     }
-                }   
+                } else {
+                    continue;
+                }
             }
+        }
+        if (start_of_filename == 0 && redirect_detected) {
+            redirect_detected = false;
+            write(STDERR_FILENO, "Redirection misformatted.\n", 26);
+            return;
+        }
+        // redirect detected, so extract name of file and handle FDs
+
+        if (redirect_detected) {
             char filename[128];
             int i;
             // extract name of batch file
             for (i = start_of_filename; i < strlen(dup_command); i++) {
+                if (dup_command[i] == ' ') {
+                    write(STDERR_FILENO, "Redirection misformatted.\n", 26);
+                    return;
+                }
                 filename[i-start_of_filename] = dup_command[i];
             }
             filename[i] = '\0';
             // attempt to open file
-            fd = open(filename, O_RDWR);
-            if (fd == -1) {
+            //printf("attempt to open file %s\n", filename);
+            fp2 = fopen(filename, "w");
+            if (fp2 == NULL) {
                 write(STDERR_FILENO, "Cannot write to file ", 21);
                 write(STDERR_FILENO, filename, strlen(filename));
                 write(STDERR_FILENO, "\n", 1);
             }
-            int old_stdout = dup(STDOUT_FILENO);
+            
+            old_stdout = dup(STDOUT_FILENO);
             if (old_stdout == -1) {
+                printf("old stdout failed\n");
                 return;
             }
-            int dup2_ret = dup2(fd, STDOUT_FILENO);
+        
+            int dup2_ret = dup2(fileno(fp2), STDOUT_FILENO);
             if (dup2_ret == -1) {
+                printf("dup2 return failed\n");
                 return;
             }
-            // beginning of argument parsing with strtok
-            char *argv[100];
-            char *tok = strtok(dup_command, " ");
-            int arg_index = 0; 
-            // parse command, add to argv array
-            while (tok != NULL) {
-                argv[arg_index] = tok;
-                tok = strtok(NULL, " ");
-                arg_index++;
+        }
+        // beginning of argument parsing with strtok
+        char *argv[100];
+        if (redirect_detected) {
+            dup_command[redirect_index] = '\0';
+        }
+        //printf("new dup_command: %s\n", dup_command);
+        char *tok = strtok(dup_command, " \t");
+        if (tok == NULL && redirect_detected) {
+            write(STDERR_FILENO, "Redirection misformatted.\n", 26);
+        }
+        int arg_index = 0;
+        
+        // parse command, add to argv array
+        while (tok != NULL) {
+           // printf("tok: %s\n", tok);
+            argv[arg_index] = tok;
+            tok = strtok(NULL, " \t");
+            arg_index++;
+        }
+        // set last argument to NULL
+        argv[arg_index] = NULL;
+        /*
+        for (int i = 0; i <= arg_index; i++) {
+            printf("argument: %s\n", argv[i]);
+            //printf("length: %lu\n", strlen(argv[i]));
+        } */
+        pid_t pid = fork();
+        if (pid == 0) {
+            //printf("I'm a child with pid %d.\n", getpid());
+            if (execv(argv[0], argv) == -1) {
+                write(STDERR_FILENO, argv[0], strlen(argv[0]));
+                write(STDERR_FILENO, ": Command not found.\n", 21);
             }
-            // set last argument to NULL
-            argv[arg_index] = NULL;
-            execv(argv[0], argv);
             //printf("child: exec failed\n");
+            //printf("execv returned! errno is [%d]\n",errno);
             free(dup_command);
             _exit(1);
         } else {
@@ -97,7 +136,13 @@ void handle_command(const char *user_command){
             //printf("I'm a parent with pid %d.\n", getpid());
             int status;
             waitpid(pid, &status, 0);  
+            if (redirect_detected) {
+                dup2(old_stdout, STDOUT_FILENO);
+                fclose(fp2);
+            }
             //printf("parent: child process exits\n");
+            
+            //printf("parent: child process exits.\n");
         }
     }
 }
@@ -105,11 +150,11 @@ void handle_command(const char *user_command){
 int main(int argc, char *argv[]) {
     // run in interactive mode
     if (argc == 1) {
-        fp = stdin;
+        fp1 = stdin;
         char str[128]; 
         while (1) {
             write(STDOUT_FILENO, "mysh> ", 6);
-            if (fgets(str, 128, fp) != NULL) {
+            if (fgets(str, 128, fp1) != NULL) {
                     if (strncmp(str, "exit", 4) == 0) {
                         break;
                     }
@@ -121,9 +166,9 @@ int main(int argc, char *argv[]) {
     }
      // run in batch mode
     else if (argc == 2) {
-        fp = fopen(argv[1], "r");
+        fp1 = fopen(argv[1], "r");
         // batch file invalid
-        if (fp == NULL) {
+        if (fp1 == NULL) {
             write(STDERR_FILENO, "Error: Cannot open file ", 24);
             write(STDERR_FILENO, argv[1], strlen(argv[1]));
             write(STDERR_FILENO, ".\n", 2);
@@ -131,15 +176,8 @@ int main(int argc, char *argv[]) {
         } else {
             char str[128];
             char* command;
-            char* echo_prefix = "echo ";
-            char* echo_command;
-            while ((command = fgets(str, 128, fp)) != NULL) {
-                echo_command = (char*) malloc(sizeof(char)*(strlen(command) + 5));
-                strcat(echo_command, echo_prefix);
-                strcat(echo_command, command);
-                system(echo_command);
-                free(echo_command);
-                //write(STDOUT_FILENO, command, strlen(command));
+            while ((command = fgets(str, 128, fp1)) != NULL) {
+                write(STDOUT_FILENO, command, strlen(command));
                 if (strncmp(command, "exit", 4) == 0) {
                     break;
                 }
@@ -152,6 +190,3 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
-
-            
-    
